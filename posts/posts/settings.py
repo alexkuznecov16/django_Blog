722567baf418d82d91c85916f936c50ef6c8186b
Alex Kuznecov <alexander.kuznecov16@gmail.com>
2024-06-13 12:45:16 +0300

    rg

--- posts/posts/settings.py
+++ posts/posts/settings.py
@@ -112,15 +112,12 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-from decouple import config
-
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'alexander.kuznecov16@gmail.com'
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'flwvgccyigmnqhgt'
 
 
 # For static file reload
